cfcccce6a3da471e2bfd915b4b4cbe68a9cf7906
skkad <skkad1998@gmail.com>
2022-06-30 20:12:43 +0530

    commit 3.0

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+          <h2 data-ns-test="project-name">Clone of an E-commerce site</h2>
+          <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro delectus ex quaerat esse maxime qui, obcaecati a quam et quis sapiente rem impedit inventore voluptas omnis quod eum repellendus cupiditate.</p>
+    </div>
   )
 }
 

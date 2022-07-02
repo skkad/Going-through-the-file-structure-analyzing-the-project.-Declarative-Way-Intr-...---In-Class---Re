be7418981812daf2291a5a41d7d8daeb69cede7f
skkad <skkad1998@gmail.com>
2022-07-02 12:05:34 +0530

    function commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
+import { render } from "enzyme";
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+
+
+function Project(){
+  return(
+    <div>
+    <h1 data-ns-test="project-name">Clone of an E-commerce site</h1>
+    <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro delectus ex quaerat esse maxime qui, obcaecati a quam et quis sapiente rem impedit inventore voluptas omnis quod eum repellendus cupiditate.</p>
+    </div>
+  );
+}
 const App = () => {
   return (
     <div id="main">
-          <h1 data-ns-test="project-name">Clone of an E-commerce site</h1>
-          <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro delectus ex quaerat esse maxime qui, obcaecati a quam et quis sapiente rem impedit inventore voluptas omnis quod eum repellendus cupiditate.</p>
+          <Project></Project>
     </div>
   )
 }

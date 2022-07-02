5e4b05b8a3af75a8e2575e3aa2758209445bb145
skkad <skkad1998@gmail.com>
2022-07-02 11:57:55 +0530

    babel commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-          <h2 data-ns-test="project-name">Clone of an E-commerce site</h2>
+          <h1 data-ns-test="project-name">Clone of an E-commerce site</h1>
           <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro delectus ex quaerat esse maxime qui, obcaecati a quam et quis sapiente rem impedit inventore voluptas omnis quod eum repellendus cupiditate.</p>
     </div>
   )
